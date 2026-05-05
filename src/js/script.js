c948af15ead576c45689ae71d589fa164d7b1286
Henrique <henriquegiusti.07@gmail.com>
2026-05-05 09:57:39 -0300

    style:esilizando

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,13 @@
+window.onload = function(){
+    const intro = document.getElementById("inicio");
+    setTimeout(()=>{
+        intro.classList.add("efeito-out");
+        setTimeout(()=>{
+            intro.style.display="none";
+            content.classList.add("efeito-in");
+        },1000)
+    },2000)
+}
 function trocar(cor){
  document.body.style.background=cor;
 }
