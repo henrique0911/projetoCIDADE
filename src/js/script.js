e0e5f905d04a25046b84fadfff7d95bbfa50c5e4
Henrique <henriquegiusti.07@gmail.com>
2026-05-19 09:37:43 -0300

    feat: funcionalidades do menu hamburguer

--- src/js/script.js
+++ src/js/script.js
@@ -11,3 +11,16 @@ window.onload = function(){
 function trocar(cor){
  document.body.style.background=cor;
 }
+
+/* menu hamburguer */
+
+document.addEventListener('DOMContentLoaded',()=>{
+    const menuIcone = document.getElementById('menu-icone');
+    const navMenu = document.querySelector('nav');
+    if (menuIcone && navMenu){
+        menuIcone.onclick=()=>{
+            navMenu.classList.toggle('active');
+            menuIcone.classList.toggle('open');
+        }
+    }
+})
